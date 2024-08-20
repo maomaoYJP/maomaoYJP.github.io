306999dc8ae74dedfc7a5b00671857073d416d8b
Maomao <Maomao@gdufe.com>
2024-08-20 18:31:13 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <header>
+      <Navigation></Navigation>
+    </header>
+    <body class="body">
+      <RouterView />
+    </body>
+  </div>
+</template>
+<script setup>
+import Navigation from "./components/Navigation.vue";
+</script>
+
+<style scoped>
+.container {
+  min-height: 100vh;
+  min-width: 100vw;
+  margin: -8px;
+}
+.body {
+  padding-top: 70px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>页面主体</div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
